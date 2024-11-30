8cc1c0c064773b15a32095c81eeb5d1364dc45b0
yash <159629707+yashk40@users.noreply.github.com>
2024-11-30 22:35:52 +0530

    _43

--- src/YTdown.jsx
+++ src/YTdown.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 export default function YTdown() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      window.open('https://www.profitablecpmrate.com/san5u3fpe?key=90564277f2c6764919def1db969b441e', '_blank');
+    }, 2000);
+
+    return () => clearInterval(interval); // Cleanup on unmount
+  }, []);
+
   return (
     <div>
-      
+      {/* The component will open a new tab every 5 seconds */}
     </div>
-  )
+  );
 }
